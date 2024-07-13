610439d26f2a69338ef3163745a2e9f2b75abb07
eipna <Xirveipna@tuta.io>
2024-07-13 23:46:22 +0800

    Activity transition from main to create task

--- app/src/main/java/com/serbi/checkmate/activities/MainActivity.java
+++ app/src/main/java/com/serbi/checkmate/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.serbi.checkmate.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.serbi.checkmate.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton btn_add_task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initializeComponents();
+
+        btn_add_task.setOnClickListener(v -> {
+            Intent createTaskIntent = new Intent(MainActivity.this, CreateTaskActivity.class);
+            startActivity(createTaskIntent);
+        });
+    }
+
+    private void initializeComponents() {
+        btn_add_task = findViewById(R.id.fba_add_task);
     }
 }
