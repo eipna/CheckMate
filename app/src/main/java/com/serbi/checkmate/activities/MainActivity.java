40d130bbf2bf82ec70a49416309155ee1e4f92b6
eipna <Xirveipna@tuta.io>
2024-07-14 02:08:01 +0800

    Added options menu in main activity

--- app/src/main/java/com/serbi/checkmate/activities/MainActivity.java
+++ app/src/main/java/com/serbi/checkmate/activities/MainActivity.java
@@ -2,20 +2,25 @@ package com.serbi.checkmate.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.serbi.checkmate.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton btn_add_task;
+    private MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         initializeComponents();
+        initializeToolbar();
 
         btn_add_task.setOnClickListener(v -> {
             Intent createTaskIntent = new Intent(MainActivity.this, CreateTaskActivity.class);
@@ -37,6 +43,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializeComponents() {
+        toolbar = findViewById(R.id.tb_main);
         btn_add_task = findViewById(R.id.fba_add_task);
+    }
+
+    private void initializeToolbar() {
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.options_settings) {
+            Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(settingsIntent);
+        }
+        return true;
     }
 }
