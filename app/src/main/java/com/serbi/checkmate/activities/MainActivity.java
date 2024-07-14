26915598dcb17147e17b8cf26e89927a981959e5
eipna <Xirveipna@tuta.io>
2024-07-14 17:01:45 +0800

    Added theme settings

--- app/src/main/java/com/serbi/checkmate/activities/MainActivity.java
+++ app/src/main/java/com/serbi/checkmate/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.serbi.checkmate.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,6 +10,7 @@ import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,6 +20,11 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.serbi.checkmate.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor sharedPreferencesEditor;
+
+    private String prefsTheme;
 
     private FloatingActionButton btn_add_task;
     private MaterialToolbar toolbar;
@@ -34,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         initializeComponents();
+        initializeSharedPreferences();
         initializeToolbar();
 
         btn_add_task.setOnClickListener(v -> {
@@ -43,12 +51,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializeComponents() {
+        sharedPreferences = getSharedPreferences("MINDCHECK", MODE_PRIVATE);
+        prefsTheme = sharedPreferences.getString("THEME", "System");
+
         toolbar = findViewById(R.id.tb_main);
         btn_add_task = findViewById(R.id.fba_add_task);
     }
 
     private void initializeToolbar() {
         setSupportActionBar(toolbar);
+    }
+
+    private void initializeSharedPreferences() {
+        // Handles the application theme
+        switch (prefsTheme) {
+            case "System":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+            case "Light":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case "Dark":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/com/serbi/checkmate/fragments/SettingsFragment.java
+++ app/src/main/java/com/serbi/checkmate/fragments/SettingsFragment.java
@@ -1,13 +1,64 @@
 package com.serbi.checkmate.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 import com.serbi.checkmate.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor sharedPreferencesEditor;
+
+    private ListPreference listTheme;
+
+    private String prefsTheme;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+        initializePreferences();
+        initializePreferencesValues();
+
+        listTheme.setOnPreferenceChangeListener((preference, newValue) -> {
+            String selectedTheme = (String) newValue;
+            listTheme.setSummary(selectedTheme);
+
+            switch (selectedTheme) {
+                case "System":
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                    sharedPreferencesEditor = sharedPreferences.edit();
+                    sharedPreferencesEditor.putString("THEME", "System");
+                    break;
+                case "Light":
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    sharedPreferencesEditor = sharedPreferences.edit();
+                    sharedPreferencesEditor.putString("THEME", "Light");
+                    break;
+                case "Dark":
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    sharedPreferencesEditor = sharedPreferences.edit();
+                    sharedPreferencesEditor.putString("THEME", "Dark");
+                    break;
+            }
+            sharedPreferencesEditor.apply();
+            return true;
+        });
+    }
+
+    private void initializePreferences() {
+        sharedPreferences = getActivity().getSharedPreferences("MINDCHECK", Context.MODE_PRIVATE);
+        prefsTheme = sharedPreferences.getString("THEME", "System");
+
+        listTheme = findPreference("list_theme");
+    }
+
+    private void initializePreferencesValues() {
+        listTheme.setSummary(prefsTheme);
+        listTheme.setValue(prefsTheme);
     }
 }
