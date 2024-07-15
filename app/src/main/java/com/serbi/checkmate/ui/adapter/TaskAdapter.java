3c76d8adbe5a892b506145caeced77c6e2172d2c
eipna <Xirveipna@tuta.io>
2024-07-15 21:36:27 +0800

    Added responsive clicks on task item cards

--- app/src/main/java/com/serbi/checkmate/ui/adapter/TaskAdapter.java
+++ app/src/main/java/com/serbi/checkmate/ui/adapter/TaskAdapter.java
@@ -6,8 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.checkbox.MaterialCheckBox;
 import com.google.android.material.textview.MaterialTextView;
 import com.serbi.checkmate.R;
@@ -45,6 +47,8 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
         holder.item_task_name.setText(task.getName());
         holder.item_task_notes.setText(task.getNotes());
         holder.item_task_check_box.setChecked(isTaskCompleted);
+
+        holder.item_task.setOnClickListener(v -> holder.item_task.setSelected(true));
     }
 
     @Override
@@ -56,9 +60,11 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
 
         MaterialTextView item_task_name, item_task_notes;
         MaterialCheckBox item_task_check_box;
+        MaterialCardView item_task;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            item_task = itemView.findViewById(R.id.item_task);
             item_task_name = itemView.findViewById(R.id.item_task_name);
             item_task_notes = itemView.findViewById(R.id.item_task_notes);
             item_task_check_box = itemView.findViewById(R.id.item_task_check_box);
