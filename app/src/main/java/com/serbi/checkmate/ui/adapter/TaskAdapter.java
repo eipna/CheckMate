e252ac8d6d94742332252d27cc956c0f8be4b019
eipna <Xirveipna@tuta.io>
2024-07-15 17:45:58 +0800

    Added adapter class for task model class

---
+++ app/src/main/java/com/serbi/checkmate/ui/adapter/TaskAdapter.java
@@ -0,0 +1,67 @@
+package com.serbi.checkmate.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.checkbox.MaterialCheckBox;
+import com.google.android.material.textview.MaterialTextView;
+import com.serbi.checkmate.R;
+import com.serbi.checkmate.data.model.TaskModel;
+
+import java.util.ArrayList;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<TaskModel> taskModels;
+
+    public TaskAdapter(Context context, ArrayList<TaskModel> taskModels) {
+        this.taskModels = taskModels;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_task, null);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        TaskModel task = taskModels.get(position);
+        boolean isTaskCompleted;
+        if (task.getIsCompleted() == 1) {
+            isTaskCompleted = true;
+        } else {
+            isTaskCompleted = false;
+        }
+
+        holder.item_task_name.setText(task.getName());
+        holder.item_task_notes.setText(task.getNotes());
+        holder.item_task_check_box.setChecked(isTaskCompleted);
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskModels.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        MaterialTextView item_task_name, item_task_notes;
+        MaterialCheckBox item_task_check_box;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            item_task_name = itemView.findViewById(R.id.item_task_name);
+            item_task_notes = itemView.findViewById(R.id.item_task_notes);
+            item_task_check_box = itemView.findViewById(R.id.item_task_check_box);
+        }
+    }
+}
