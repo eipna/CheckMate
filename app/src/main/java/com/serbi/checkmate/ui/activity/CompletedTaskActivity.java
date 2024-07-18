548df1d8e2448d6b3f83eebbb908cd093a2b4bd1
eipna <Xirveipna@tuta.io>
2024-07-18 22:58:56 +0800

    Completed task activity display completed tasks

--- app/src/main/java/com/serbi/checkmate/ui/activity/CompletedTaskActivity.java
+++ app/src/main/java/com/serbi/checkmate/ui/activity/CompletedTaskActivity.java
@@ -7,13 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.serbi.checkmate.R;
+import com.serbi.checkmate.data.local.AppDatabase;
+import com.serbi.checkmate.data.model.TaskModel;
+import com.serbi.checkmate.ui.adapter.TaskAdapter;
+
+import java.util.List;
 
 public class CompletedTaskActivity extends AppCompatActivity {
 
+    private List<TaskModel> taskModels;
+
+    private TaskAdapter adapter;
+    private AppDatabase appDatabase;
+
     private MaterialToolbar toolbar;
+    private RecyclerView rv_completed_task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +41,28 @@ public class CompletedTaskActivity extends AppCompatActivity {
 
         initializeComponents();
         initializeToolbar();
+        initializeDatasets();
+        displayTaskItems();
+    }
+
+    // Displays the task items in the recyclerview
+    private void displayTaskItems() {
+        adapter = new TaskAdapter(this, taskModels);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+        // Makes the task items to generate at the top of the list instead at the bottom
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        rv_completed_task.setLayoutManager(linearLayoutManager);
+        rv_completed_task.setAdapter(adapter);
     }
 
     private void initializeComponents() {
+        appDatabase = new AppDatabase(CompletedTaskActivity.this);
+
         toolbar = findViewById(R.id.tb_completed_task);
+        rv_completed_task = findViewById(R.id.rv_completed_task);
     }
 
     private void initializeToolbar() {
@@ -40,5 +71,10 @@ public class CompletedTaskActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+    }
+
+    private void initializeDatasets() {
+        // Gets all task items from database ( 1 means true)
+        taskModels = appDatabase.getTaskItems(1);
     }
 }
