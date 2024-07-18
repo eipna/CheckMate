da4b54b60ce0794e03844ebfdf650ccd3ef624a8
eipna <Xirveipna@tuta.io>
2024-07-18 22:26:58 +0800

    Added toolbar functions for completed task activity

--- app/src/main/java/com/serbi/checkmate/ui/activity/CompletedTaskActivity.java
+++ app/src/main/java/com/serbi/checkmate/ui/activity/CompletedTaskActivity.java
@@ -8,9 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.serbi.checkmate.R;
 
 public class CompletedTaskActivity extends AppCompatActivity {
+
+    private MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,20 @@ public class CompletedTaskActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initializeComponents();
+        initializeToolbar();
+    }
+
+    private void initializeComponents() {
+        toolbar = findViewById(R.id.tb_completed_task);
+    }
+
+    private void initializeToolbar() {
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
     }
 }
