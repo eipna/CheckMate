60616d2bc5f0ab96be9ed37b5516abb6d789fb89
eipna <Xirveipna@tuta.io>
2024-07-21 23:35:14 +0800

    Removed un-needed custom request codes in constants class

--- app/src/main/java/com/serbi/checkmate/Constants.java
+++ app/src/main/java/com/serbi/checkmate/Constants.java
@@ -4,8 +4,4 @@ public final class Constants {
     // Task status
     public static final int TASK_COMPLETED = 1;
     public static final int TASK_NOT_COMPLETED = 0;
-
-    // Custom request codes
-    public static final int CREATE_TASK_REQUEST_CODE = 420;
-    public static final int EDIT_TASK_REQUEST_CODE = 421;
 }
