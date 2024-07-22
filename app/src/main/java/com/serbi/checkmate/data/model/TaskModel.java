dc7a06a889b281ab072ca5385cbc0a7fae52a247
eipna <Xirveipna@tuta.io>
2024-07-22 22:31:38 +0800

    Added created variable for task model

--- app/src/main/java/com/serbi/checkmate/data/model/TaskModel.java
+++ app/src/main/java/com/serbi/checkmate/data/model/TaskModel.java
@@ -5,12 +5,14 @@ public class TaskModel {
     private int id;
     private String name;
     private String notes;
+    private long dateCreated;
     private int isCompleted;
 
-    public TaskModel(int id, String name, String notes, int isCompleted) {
+    public TaskModel(int id, String name, String notes, long dateCreated, int isCompleted) {
         this.id = id;
         this.name = name;
         this.notes = notes;
+        this.dateCreated = dateCreated;
         this.isCompleted = isCompleted;
     }
 
@@ -44,5 +46,13 @@ public class TaskModel {
 
     public void setIsCompleted(int isCompleted) {
         this.isCompleted = isCompleted;
+    }
+
+    public void setDateCreated(long dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public long getDateCreated() {
+        return dateCreated;
     }
 }
