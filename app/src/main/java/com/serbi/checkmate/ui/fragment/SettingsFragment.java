313af0a66aae983b179671da1e9d28eabe7bcaf5
eipna <Xirveipna@tuta.io>
2024-07-23 02:27:35 +0800

    Added libraries section in settings

--- app/src/main/java/com/serbi/checkmate/ui/fragment/SettingsFragment.java
+++ app/src/main/java/com/serbi/checkmate/ui/fragment/SettingsFragment.java
@@ -1,13 +1,26 @@
 package com.serbi.checkmate.ui.fragment;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import com.serbi.checkmate.R;
+
+import java.util.Objects;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -15,6 +28,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     private SharedPreferences.Editor sharedPreferencesEditor;
 
     private ListPreference listTheme;
+    private Preference listLibraries;
 
     private String prefsTheme;
 
@@ -48,6 +62,38 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             sharedPreferencesEditor.apply();
             return true;
         });
+
+        listLibraries.setOnPreferenceClickListener(preference -> {
+            showLibrariesListDialog();
+            return true;
+        });
+    }
+
+    // Shows the dialog of list of libraries
+    private void showLibrariesListDialog() {
+        String[] librariesItems = getResources().getStringArray(R.array.list_libraries);
+        View listViewContainer = getLayoutInflater().inflate(R.layout.dialog_libraries_list, null);
+        ListView librariesListView = listViewContainer.findViewById(R.id.listview_libraries);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_list_item_1, librariesItems);
+        librariesListView.setAdapter(adapter);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        builder.setTitle("Libraries").
+                setView(listViewContainer)
+                .setPositiveButton("Cancel", null);
+
+        librariesListView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedLibrary = librariesItems[position];
+            Intent browserIntent = null;
+
+            switch (selectedLibrary) {
+                case "PrettyTime":
+                    browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.webite_pretty_time)));
+            }
+            startActivity(browserIntent);
+        });
+
+        builder.create().show();
     }
 
     private void initializePreferences() {
@@ -55,6 +101,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         prefsTheme = sharedPreferences.getString("THEME", "System");
 
         listTheme = findPreference("list_theme");
+        listLibraries = findPreference("list_libraries");
     }
 
     private void initializePreferencesValues() {
