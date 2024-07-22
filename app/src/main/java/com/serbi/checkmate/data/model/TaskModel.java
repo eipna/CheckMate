7643f68a7cbfbc8117c4b781721d259a9b64a829
eipna <Xirveipna@tuta.io>
2024-07-22 22:33:12 +0800

    Made task model instance variables final

--- app/src/main/java/com/serbi/checkmate/data/model/TaskModel.java
+++ app/src/main/java/com/serbi/checkmate/data/model/TaskModel.java
@@ -2,11 +2,11 @@ package com.serbi.checkmate.data.model;
 
 public class TaskModel {
 
-    private int id;
-    private String name;
-    private String notes;
-    private long dateCreated;
-    private int isCompleted;
+    private final int id;
+    private final String name;
+    private final String notes;
+    private final long dateCreated;
+    private final int isCompleted;
 
     public TaskModel(int id, String name, String notes, long dateCreated, int isCompleted) {
         this.id = id;
