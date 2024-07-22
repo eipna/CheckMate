9d9b66cfce4024e71b1b90c4d7d46aae70ec7d74
eipna <Xirveipna@tuta.io>
2024-07-22 22:29:48 +0800

    Added DateHandler class

---
+++ app/src/main/java/com/serbi/checkmate/util/DateHandler.java
@@ -0,0 +1,9 @@
+package com.serbi.checkmate.util;
+
+public class DateHandler {
+
+    // Returns current time stamp
+    public static long getCurrentTimeStamp() {
+        return System.currentTimeMillis();
+    }
+}
