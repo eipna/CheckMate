7b8d7e621f876522933c5f748105f4b4cdde8394
eipna <Xirveipna@tuta.io>
2024-07-22 22:58:59 +0800

    Added current detailed date getter

--- app/src/main/java/com/serbi/checkmate/util/DateHandler.java
+++ app/src/main/java/com/serbi/checkmate/util/DateHandler.java
@@ -1,9 +1,22 @@
 package com.serbi.checkmate.util;
 
+import android.annotation.SuppressLint;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateHandler {
 
     // Returns current time stamp
     public static long getCurrentTimeStamp() {
         return System.currentTimeMillis();
+    }
+
+    // Returns a detailed format of the current date (Ex. Monday, 22 July 2024)
+    public static String getDetailedDate() {
+        Date currentDate = new Date(getCurrentTimeStamp());
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat =
+                new SimpleDateFormat("EEEE, dd MMMM yyyy");
+        return simpleDateFormat.format(currentDate);
     }
 }
