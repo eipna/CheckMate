2e0e061548b5a255c287cbcbd2970c18cb52c154
eipna <Xirveipna@tuta.io>
2024-07-23 12:38:56 +0800

    Added comments for interfaces

--- app/src/main/java/com/serbi/checkmate/data/interfaces/TaskListener.java
+++ app/src/main/java/com/serbi/checkmate/data/interfaces/TaskListener.java
@@ -1,6 +1,6 @@
 package com.serbi.checkmate.data.interfaces;
 
 public interface TaskListener {
-    void onTaskClick(int position);
-    void onTaskCheck(int position, boolean status);
+    void onTaskClick(int position); // On task item click
+    void onTaskCheck(int position, boolean status); // On task item check box click
 }
