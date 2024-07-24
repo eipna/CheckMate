c84c40db07cec0474cd2dbae4ea8970fc21cfd1c
eipna <Xirveipna@tuta.io>
2024-07-25 01:53:20 +0800

    Migrate use view binding in settings activity

--- app/src/main/java/com/serbi/checkmate/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/serbi/checkmate/ui/activity/SettingsActivity.java
@@ -8,43 +8,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.appbar.MaterialToolbar;
 import com.serbi.checkmate.R;
+import com.serbi.checkmate.databinding.ActivitySettingsBinding;
 import com.serbi.checkmate.ui.fragment.SettingsFragment;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private MaterialToolbar toolbar;
+    private ActivitySettingsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_settings);
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.tbSettings); // Initialize toolbar
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        initializeSettings();
-        initializeComponents();
-        initializeToolbar();
-    }
-
-    private void initializeComponents() {
-        toolbar = findViewById(R.id.tb_settings);
-    }
-
-    private void initializeToolbar() {
-        setSupportActionBar(toolbar);
+        // Setup toolbar
+        setSupportActionBar(binding.tbSettings);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
-    }
 
-    private void initializeSettings() {
+        // Inflates preference settings
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.container_settings, new SettingsFragment())
