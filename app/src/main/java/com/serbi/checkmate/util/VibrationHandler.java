8563d593116a9e6a49518707274c43f057452e11
eipna <Xirveipna@tuta.io>
2024-07-25 14:40:52 +0800

    Added vibration handler class in util package

---
+++ app/src/main/java/com/serbi/checkmate/util/VibrationHandler.java
@@ -0,0 +1,18 @@
+package com.serbi.checkmate.util;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+
+public class VibrationHandler {
+
+    public static void vibrate(Context context, int duration) {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            vibrator.vibrate(duration);
+        }
+    }
+}
