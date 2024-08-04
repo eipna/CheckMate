c9654faf4b89a93099b743e6c8ec1c79ca643302
eipna <Xirveipna@tuta.io>
2024-08-04 16:17:49 +0800

    Updated TaskListener interface

--- app/src/main/java/com/serbi/checkmate/data/interfaces/TaskListener.java
+++ app/src/main/java/com/serbi/checkmate/data/interfaces/TaskListener.java
@@ -1,6 +1,8 @@
 package com.serbi.checkmate.data.interfaces;
 
+import android.view.View;
+
 public interface TaskListener {
     void onTaskClick(int position); // On task item click
-    void onTaskCheck(int position, boolean status); // On task item check box click
+    void onTaskMoreOptionsClick(int position, View view); // On task item more options button click
 }
